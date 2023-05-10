0d05d61c7b2dd21b08f4b580ecc2af8d829ea573
saf94 <safeer.hayat@beryl.cc>
2023-05-10 18:50:11 +0100

    format date

--- src/components/Launches/Launches.tsx
+++ src/components/Launches/Launches.tsx
@@ -27,28 +27,32 @@ const Launches = (): JSX.Element => {
   if (!data) return <p>Error: no launches found</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  console.log("data", data);
-
   return (
     <>
       <h1>Space X Rocket Launches History</h1>
       <section className="launchesSection">
         {data.launchesPast.map(
-          ({ id, mission_name, launch_date_utc, rocket, links }) => (
-            <div className="launchContainer" key={id}>
-              <div className="launchHeader">
-                <h3 className="missionName">{mission_name}</h3>
-                <p>Date of launch: {launch_date_utc}</p>
+          ({ id, mission_name, launch_date_utc, rocket, links }) => {
+            const formattedDate = new Date(launch_date_utc).toLocaleDateString(
+              "en-gb"
+            );
+            return (
+              <div className="launchContainer" key={id}>
+                <div className="launchHeader">
+                  <h3 className="missionName">{mission_name}</h3>
+                  <p>Date of launch: {formattedDate}</p>
+                </div>
+                <p>Name of rocket: {rocket.rocket_name}</p>
+                <p>
+                  Article link:{" "}
+                  <a href={links.article_link}>{links.article_link}</a>
+                </p>
+                <p>
+                  Video link: <a href={links.video_link}>{links.video_link}</a>
+                </p>
               </div>
-              <p>Name of rocket: {rocket.rocket_name}</p>
-              <p>
-                Article link: <a href={links.article_link}>{links.article_link}</a>
-              </p>
-              <p>
-                Video link: <a href={links.video_link}>{links.video_link}</a>
-              </p>
-            </div>
-          )
+            );
+          }
         )}
       </section>
     </>
